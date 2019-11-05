33d933b85c6b114db33d8f02bc09de1d22049c17
Colin Lin <colincool100@gmail.com>
2019-11-05 15:12:34 -0500

    ghost

--- PEAR/src/pages/about.js
+++ PEAR/src/pages/about.js
@@ -1,9 +1,13 @@
 import React from "react"
+import Header from "../components/header"
+import { Link } from "gatsby"
 
 export default () => (
   <div style={{ color: `teal` }}>
+    <Header siteTitle = "lololoo"/>
     <h1>About Gatsby</h1>
     <p>Such wow. Very React.</p>
-    <button class="primary-button">Click me</button>
+    <Link to="/"><button class="primary-button" >Click me</button></Link>
+
   </div>
 )
