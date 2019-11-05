53a6dba1bde8e134522de835a827e24b9f25ae10
Colin Lin <colincool100@gmail.com>
2019-11-05 14:40:50 -0500

    beat henry

--- PEAR/src/pages/index.js
+++ PEAR/src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>We have to beat Henry</h1>
+    <p>this is the index page</p>
+    <p>$250 on the line</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
