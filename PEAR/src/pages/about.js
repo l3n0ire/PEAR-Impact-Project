fe57cbdac59a97f9a921bf65d3e0210ee1235b6d
Colin Lin <colincool100@gmail.com>
2019-11-05 14:45:59 -0500

    about

---
+++ PEAR/src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <h1>About Gatsby</h1>
+    <p>Such wow. Very React.</p>
+    <button class="primary-button">Click me</button>
+  </div>
+)
