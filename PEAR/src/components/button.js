56a7a880251973bb0fd1b465180b441cd3c5d77a
Mista4444 <mista4444@users.noreply.github.com>
2019-11-06 11:49:58 -0500

    Button Sylesheet
    - Moved the button styling into a seperate .css file

--- PEAR/src/components/button.js
+++ PEAR/src/components/button.js
@@ -1,14 +1,9 @@
 import PropTypes from "prop-types"
 import React from "react"
 
+import "./button.css"
+
 const Button = ({ buttonText}) => (
-    <button
-    style = {{
-        background: '#333',
-        color:'white',
-        fontFamily: '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto',
-        padding: '0.5%'
-    }}
-    > {buttonText}</button>
+    <button> {buttonText}</button>
 )
 export default Button
