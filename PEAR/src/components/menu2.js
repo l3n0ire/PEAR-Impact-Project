409d1be3657e9e30663ac2ad5ed21e5ba5bea1ea
Colin Lin <colincool100@gmail.com>
2019-11-12 16:12:03 -0500

    search bar

--- PEAR/src/components/menu2.js
+++ PEAR/src/components/menu2.js
@@ -4,19 +4,23 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Link from 'gatsby-link'
 
 import { createMuiTheme } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+import { fade} from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import InputBase from '@material-ui/core/InputBase';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+
 
 const theme = createMuiTheme({
   palette: {
-    
     primary:{
-      light: '#002884',
+       
       main: '#333',
-      dark: '#002884',
       contrastText: '#fff',
     }
     ,
@@ -37,6 +41,41 @@ const useStyles = makeStyles(theme => ({
   title: {
     flexGrow: 1,
   },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+  },
+
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
+  
+  
+
   
 }));
 
@@ -52,6 +91,20 @@ const Menu2 = () => {
           <Typography color="inherit" variant="h6" className={classes.title} component={Link} to='/'>
             <strong> Pear Impact Project</strong>
           </Typography>
+
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
 
           <Button color="secondary" href="/about">
             <strong> About</strong>
