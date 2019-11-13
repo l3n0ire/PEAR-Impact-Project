6d06d470cb468b28b075927340c8c53821fa3b59
Colin Lin <colincool100@gmail.com>
2019-11-13 14:28:32 -0500

    idk

--- PEAR/src/pages/blog.js
+++ PEAR/src/pages/blog.js
@@ -1,11 +1,43 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import { Grid, Box, Grommet, Text } from "grommet"
 
 
 
 const BlogPage = ({data}) => (
   <Layout>
+
+<h1>Latest Posts</h1>
+    <Grid container
+    rows = {['15vh', '65vh', '10vh', '20vh']}
+    columns = {['full']}
+    areas = {[
+      { name: 'header', start: [0, 0], end: [0, 0] },
+      { name: 'content', start: [0, 1], end: [0, 1] },
+      { name: 'footer', start: [0, 3], end: [0, 3] }
+
+    ]}
+    >
+          {data.allMarkdownRemark.edges.map(post =>(
+            <Grid item key={post.node.id}>
+              <Card>
+                  <CardContent>
+                  <Text variant="h5">{post.node.frontmatter.title}</Text>
+                  <Text color="textSecondary">
+                    Posted by {post.node.frontmatter.author} on {post.node.frontmatter.date}
+                  </Text>
+                  <br/><br/>
+                  <Link to ={post.node.frontmatter.path}>
+                    Read more
+                  </Link>
+                  <br/><br/>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+
+    </Grid>
 
   </Layout>
 )
