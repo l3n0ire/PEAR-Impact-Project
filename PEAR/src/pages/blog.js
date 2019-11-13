e97e5694d75f2373ae0459dc1ecd8ef40a5dd07d
Colin Lin <colincool100@gmail.com>
2019-11-13 15:26:35 -0500

    graphql

--- PEAR/src/pages/blog.js
+++ PEAR/src/pages/blog.js
@@ -2,22 +2,31 @@ import React from "react"
 
 import Layout from "../components/layout"
 import { Grid, Box, Grommet, Text, InfiniteScroll } from "grommet"
+import Link from "gatsby-link"
 
 
 
 const BlogPage = ({data}) => (
   <Layout>
-    <Grid columns = 'medium' rows = 'medium' alignContent = 'center'>
-        <InfiniteScroll items = {['1','2','3', '4']}>
-          {(item) => (
-            <Box justify = 'center'>
-              <Text alignSelf = 'center'>{item}</Text>
-            </Box>
-          )          
-          }
+        
 
-        </InfiniteScroll>  
+    <Grid columns = 'medium' rows = 'medium' alignContent = 'center' gap="large" margin='large'>
+
+        
+    {data.allMarkdownRemark.edges.map(post =>(
+            <Box justify = 'center' border = 'all'>
+              <Text alignSelf = 'center'> {post.node.frontmatter.title}</Text>
+              <Text alignSelf ='center'> Posted by {post.node.frontmatter.author} on {post.node.frontmatter.date}</Text>
+              <hr></hr>
+              <Link to ={post.node.frontmatter.path}>
+                    Read more
+                  </Link>
+
+            </Box>
+                 ))}
+
       </Grid>
+
   </Layout>
 )
 
